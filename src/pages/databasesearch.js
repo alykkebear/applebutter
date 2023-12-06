31472365423263144b54563b1943b4cb7e20b23b
Aniva Bjorn <bearaknightly@gmail.com>
2023-12-06 11:43:58 -1000

    hopefully this works

--- src/pages/databasesearch.js
+++ src/pages/databasesearch.js
@@ -3,11 +3,13 @@ import Link from 'next/link';
 export default function FirstPost() {
     return (
       <>
-        <h1> Second Post</h1>
-        <h2>
-          <Link href="/">Back to home</Link>
-          <input type="text" placeholder="Type database" className="input input-bordered justify-center w-full max-w-xs" />
-        </h2>
+        <main className= "flex min-h-screen flex-col items-center justify-center bg-[#274472]">
+          <h1> Second Post</h1>
+          <h2>
+            <Link href="/">Back to home</Link>
+            <input type="text" placeholder="Type database" className="input input-bordered justify-center w-full max-w-xs" />
+          </h2>
+        </main>
       </>
     );
   }
