f5e7aa601c0ae446c4b61f085d9a741491896d30
7kkeen <2421200591@k12.hi.us>
2023-12-06 11:19:38 -1000

    text box

--- src/pages/databasesearch.js
+++ src/pages/databasesearch.js
@@ -6,6 +6,7 @@ export default function FirstPost() {
         <h1> Second Post</h1>
         <h2>
           <Link href="/">Back to home</Link>
+          <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
         </h2>
       </>
     );
