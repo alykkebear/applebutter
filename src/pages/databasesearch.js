c7634ab931f47f8b12fa4743651dc7191b7bc131
7kkeen <2421200591@k12.hi.us>
2023-12-06 11:33:46 -1000

    center text box

--- src/pages/databasesearch.js
+++ src/pages/databasesearch.js
@@ -6,7 +6,7 @@ export default function FirstPost() {
         <h1> Second Post</h1>
         <h2>
           <Link href="/">Back to home</Link>
-          <input type="text" placeholder="Type database" className="input input-bordered w-full max-w-xs" />
+          <input type="text" placeholder="Type database" className="input input-bordered justify-center w-full max-w-xs" />
         </h2>
       </>
     );
