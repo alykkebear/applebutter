2d2f5687563baf13d3e09177ba28d9ed158f7f35
Aniva Bjorn <bearaknightly@gmail.com>
2023-12-06 11:36:37 -1000

    nit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#C3E0E5] to-[#274472]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-mono tracking-tight text-white sm:text-[5rem]">
-            Archive <span className="text-[hsl(280,100%,70%)]">3</span> Back in Black
+            Archive <span className="text-[#274472]">3</span> Back in Black
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
