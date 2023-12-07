1a122f1b7919bb8e44f8ccc77c190f7ff080768d
Aniva Bjorn <bearaknightly@gmail.com>
2023-12-07 13:05:11 -1000

    daise

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,11 @@ export default function Home() {
   return (
     <>
       <html data-theme="retro">
+        <ul className="menu bg-base-200 w-56 rounded-box">
+         <li><a>Item 1</a></li>
+          <li><a>Item 2</a></li>
+          <li><a>Item 3</a></li>
+        </ul>
         <Head>
           <title>Archive 3: Back in Black</title>
           <meta name="Game Title" content="super cringy thing that i couldn't come up with" />
