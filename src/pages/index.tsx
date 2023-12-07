bbf03496107e2b5e95085791c29fac0afaf8281b
Aniva Bjorn <bearaknightly@gmail.com>
2023-12-07 13:37:00 -1000

    tweaking stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
        <Head data-theme="retro">
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-neutral text-neutral-content">
          <div className="navbar-start">
           <div className="dropdown">
            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
@@ -23,44 +23,19 @@ export default function Home() {
           </div>
          </div>
          <div className="navbar-center">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">Archive 3</a>
          </div>
           <div className="navbar-end">
           </div>
         </div>
-        <title>Archive 3: Back in Black</title>
-        <meta name="Game Title" content="super cringy thing that i couldn't come up with" />
-        <link rel="icon" href="/favicon.ico" />
        </Head>
        <main data-theme="retro" className="flex min-h-screen flex-col items-center justify-center">
          <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-mono tracking-tight text-white sm:text-[5rem]">
+           <h1 className="text-5xl font-mono tracking-tight text-white sm:text-[5rem]">
             Archive 3 Back in Black
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-             <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4  hover:bg-white/20"
-              href="/fileupload"
-              target="_blank"
-             >
-               <h3 className="text-2xl font-mono">File Upload →</h3>
-               <div className="text-lg">
-                 a nice starter pack- and certainly cooler than that other page :
-               </div>
-             </Link>
-            <Link
-               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4  hover:bg-white/20"
-               href="/databasesearch"
-               target="_blank"
-            >
-               <h3 className="text-2xl font-mono">Database →</h3>
-               <div className="text-lg">
-                Welcome to Kaui's page ^-^
-               </div>
-            </Link>
-           </div>
-         </div>
-      </main>
+           </h1>
+          </div>
+       </main>
     </>
   );
 }
