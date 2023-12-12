fed44f43f8e3e72fce7ae4de17cf3984a336e977
Aniva Bjorn <bearaknightly@gmail.com>
2023-12-12 12:30:47 -1000

    and aalllllll stupid things i do

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
      
     
-        <div className="navbar bg-neutral text-neutral-content">
+        <div className="navbar bg-black text-neutral-content">
          <div className="navbar-start">
           <div className="dropdown">
            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
