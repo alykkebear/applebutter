623168d998eb29c1779ff3e47d27fa07bccb1894
Aniva Bjorn <bearaknightly@gmail.com>
2023-12-12 11:38:53 -1000

    please work this time

--- src/pages/fileupload.js
+++ src/pages/fileupload.js
@@ -1,16 +1,32 @@
 import Link from 'next/link';
 
-export default function FirstPost() {
+export default function fileuploadpage() {
   return (
     <>
-      
-      <main className= "flex min-h-screen flex-col items-center justify-center bg-[#C3E0E5]">
-        <h1>First Post</h1>
-        <h2>
-          <Link href="/">Back to home</Link>
-          <input type="file" className="file-input file-input-bordered bg-[#C3E0E5] justify-center w-full max-w-xs" />
-        </h2>
-     </main>
+    //navbar
+      <main>
+        <div className="navbar bg-neutral text-neutral-content">
+         <div className="navbar-start">
+          <div className="dropdown">
+           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+           </div>
+           <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <li><a><Link href = "./playgame" target = "_blank">Play</Link></a></li>
+            <li><a><Link href = "./fileupload" target = "_blank">File Upload</Link></a></li>
+            <li><a><Link href = "./databasesearch" target = "_blank">Database</Link></a></li>
+           </ul>
+          </div>
+         </div>
+         <div className="navbar-center">
+          <a className="btn text-xl font-mono">Archive 3</a>
+         </div>
+          <div className="navbar-end">
+          </div>
+        </div>
+      </main>
     </>
+     
+  
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,14 +16,14 @@ export default function Home() {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
            </div>
            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>Play</a></li>
+            <li><a><Link href = "./playgame" target = "_blank">Play</Link></a></li>
             <li><a><Link href = "./fileupload" target = "_blank">File Upload</Link></a></li>
-            <li><a><Link href = "./databasearch" target = "_blank">Database</Link></a></li>
+            <li><a><Link href = "./databasesearch" target = "_blank">Database</Link></a></li>
            </ul>
           </div>
          </div>
          <div className="navbar-center">
-          <a className="btn btn-ghost text-xl">Archive 3</a>
+          <a className="btn text-xl font-mono">Archive 3</a>
          </div>
           <div className="navbar-end">
           </div>
