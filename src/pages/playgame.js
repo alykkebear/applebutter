db1eda5c79cf060648c53db72b31fc6222294b48
Aniva Bjorn <bearaknightly@gmail.com>
2024-01-08 14:17:19 -1000

    for taking a chance on me

--- src/pages/playgame.js
+++ src/pages/playgame.js
@@ -1,10 +1,28 @@
 import Link from 'next/link';
-import Navbar from './navbar.js'
+import React from 'react';
 export default function playgamepage() {
   return (
     <>
       <main>
-        <Navbar />
+      <div className="navbar bg-neutral text-neutral-content">
+         <div className="navbar-start">
+          <div className="dropdown">
+           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+           </div>
+           <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <li><a><Link href = "./playgame" target = "_blank">Play</Link></a></li>
+            <li><a><Link href = "./fileupload" target = "_blank">File Upload</Link></a></li>
+            <li><a><Link href = "./databasesearch" target = "_blank">Database</Link></a></li>
+           </ul>
+          </div>
+         </div>
+         <div className="navbar-center">
+          <a className="font-mono">Archive 3</a>
+         </div>
+          <div className="navbar-end">
+          </div>
+        </div>
         <div className="skeleton w-320 h-320"></div>
      </main>
     </>
--- src/pages/siteNavbar.tsx
+++ src/pages/siteNavbar.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import react from 'react';
+import React from 'react';
 
 function siteNavbar() {
     return (
