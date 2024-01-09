5d034504d96f04dbd79ed14cd91ddbae493b8d99
Aniva Bjorn <bearaknightly@gmail.com>
2024-01-08 14:32:40 -1000

    fine ill stop doing lyrics

--- src/pages/navbar.tsx
+++ src/pages/navbar.tsx
@@ -1,30 +1,29 @@
 import Link from 'next/link';
 import React from 'react';
-export default function test() {
+export default function Navbar() {
   return (
     <>
-      <main data-theme="retro">
-      <ul className="menu bg-base-200 w-56 rounded-box">
-  <li>
-    <a>
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
-      <Link href = "./playgame" target = "_blank">Play</Link>
-    </a>
-  </li>
-  <li>
-    <a>
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-      <Link href = "./fileupload" target = "_blank">File Upload</Link>
-    </a>
-  </li>
-  <li>
-    <a>
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>
-      <Link href = "./databasesearch" target = "_blank">Database</Link>
-    </a>
-  </li>
-</ul>
-      </main>
+      <main>
+      <div className="navbar bg-neutral text-neutral-content">
+         <div className="navbar-start">
+          <div className="dropdown">
+           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+           </div>
+           <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <li><a><Link href = "./playgame" target = "_blank">Play</Link></a></li>
+            <li><a><Link href = "./fileupload" target = "_blank">File Upload</Link></a></li>
+            <li><a><Link href = "./databasesearch" target = "_blank">Database</Link></a></li>
+           </ul>
+          </div>
+         </div>
+         <div className="navbar-center">
+          <a className="font-mono">Archive 3</a>
+         </div>
+          <div className="navbar-end">
+          </div>
+        </div>
+     </main>
     </>
      
   
