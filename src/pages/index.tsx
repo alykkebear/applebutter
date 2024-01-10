7ca7a9f3dd5b71f2886c1c4279014f1133b10a4d
Aniva Bjorn <bearaknightly@gmail.com>
2024-01-10 11:39:11 -1000

    more colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,16 +8,14 @@ export default function Home() {
 
   return (
     <>
-       <Head data-theme="retro" >
-        <div className = "bg-primary">
+       <Head data-theme="retro" class-name = "bg-primary" >
         <Navbar />
         <p className = "justify-center font-mono">Explore the ruins of archiveterra while making your stamp on history</p>
          <div className="flex flex-col w-full lg:flex-row bg-accent">
-          <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">upload photos to be archived</div> 
+          <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center bg-primary">upload photos to be archived</div> 
           <div className="divider lg:divider-horizontal">OR</div> 
-          <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">archive photos by playing the game</div>
+          <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center bg-secondary">archive photos by playing the game</div>
          </div>
-        </div>
        </Head>
        
     </>
