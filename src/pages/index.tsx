2bca78b8c98bc7a89a5e616ba827330a12777d9a
Aniva Bjorn <bearaknightly@gmail.com>
2024-01-10 11:36:28 -1000

    adding text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,12 @@ export default function Home() {
        <Head data-theme="retro" >
         <div className = "bg-primary">
         <Navbar />
-        <p className = "justify-center font-mono">welcome to the home page lol</p>
+        <p className = "justify-center font-mono">Explore the ruins of archiveterra while making your stamp on history</p>
+         <div className="flex flex-col w-full lg:flex-row bg-accent">
+          <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">upload photos to be archived</div> 
+          <div className="divider lg:divider-horizontal">OR</div> 
+          <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">archive photos by playing the game</div>
+         </div>
         </div>
        </Head>
        
