84fca262d15d929586012862674ccbd0b07438a3
Aniva Bjorn <bearaknightly@gmail.com>
2024-01-10 11:14:46 -1000

    fixing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { api } from "~/utils/api";
 import React from 'react';
-
+import Navbar from './navbar'
 export default function Home() {
   const hello = api.post.hello.useQuery({ text: "from tRPC" });
 
