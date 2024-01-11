5bd239671d5b218ec541500dac712f2b9ddd0613
Aniva Bjorn <bearaknightly@gmail.com>
2024-01-11 13:00:44 -1000

    please help me

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,20 +8,16 @@ export default function Home() {
 
   return (
     <>
-       <Head data-theme="retro" class-name = "bg-primary" >
+       <main data-theme="retro" class-name = "bg-primary" >
         <Navbar />
-        <label className="flex cursor-pointer gap-2">
-         <span className="label-text font-mono">eyesore</span> 
-         <input type="checkbox" value="retro" className="toggle theme-controller"/>
-         <span className="label-text font-mono">retro</span> 
-        </label>
+        
         <p className = "justify-center font-mono">Explore the ruins of archiveterra while making your stamp on history</p>
          <div className="flex flex-col w-full lg:flex-row bg-accent">
           <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center bg-primary">upload photos to be archived</div> 
           <div className="divider lg:divider-horizontal">OR</div> 
           <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center bg-secondary">archive photos by playing the game</div>
          </div>
-       </Head>
+       </main>
        
     </>
   );
