94b17fa92a386c8a0effaf7b39a702ea34112077
Aniva Bjorn <bearaknightly@gmail.com>
2024-01-11 13:35:06 -1000

    help

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,9 +12,9 @@ export default function Home() {
         <Navbar />
         <p className = "justify-center font-mono">Explore the ruins of archiveterra while making your stamp on history</p>
          <span /> <span /> <span /> <span /> <span /> <span />
-         <div className="flex flex-col gap-4 w-52 justify-center">
-          <div className="skeleton h-32 w-full"></div>
-          <div className="skeleton h-4 w-28"></div>
+         <div className="flex flex-col gap-4 w-144 justify-center">
+          <div className="skeleton h-96 w-full"></div>
+          <div className="skeleton h-4 w-100"></div>
           <div className="skeleton h-4 w-full"></div>
           <div className="skeleton h-4 w-full"></div>
          </div>
