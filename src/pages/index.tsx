c4bce912d5fc84e840289041ac26a5fdc5b6b2c2
Aniva Bjorn <bearaknightly@gmail.com>
2024-01-11 13:15:02 -1000

    skeleton in the closte

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,12 +12,12 @@ export default function Home() {
         <Navbar />
         <p className = "justify-center font-mono">Explore the ruins of archiveterra while making your stamp on history</p>
          <span /> <span /> <span /> <span /> <span /> <span />
-         <div className="flex flex-col gap-4 w-52">
-           <div className="skeleton h-32 w-full"></div>
-           <div className="skeleton h-4 w-28"></div>
-           <div className="skeleton h-4 w-full"></div>
-           <div className="skeleton h-4 w-full"></div>
-         </div>
+         <div className="flex flex-col gap-4 w-full justify-center">
+           <div className="skeleton h-96 w-144"></div>
+           <div className="skeleton h-4 w-96"></div>
+           <div className="skeleton h-4 w-69"></div>
+           <div className="skeleton h-4 w-96"></div>
+          </div>
        </main>
        
     </>
