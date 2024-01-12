efb60912a32755645853798a410c92599bc6ca89
Aniva Bjorn <bearaknightly@gmail.com>
2024-01-12 13:31:25 -1000

    woooow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <Navbar />
         <p className = "justify-self-center font-mono">Explore the ruins of archiveterra while making your stamp on history</p>
          <span /> <span /> <span /> <span /> <span /> <span />
-         <div className="flex flex-col gap-4 w-4/5 git justify-self-center">
+         <div className="flex flex-col gap-4 w-4/5 git justify-center items-center">
           <div className="skeleton h-96 w-full"></div>
           <div className="skeleton h-4 w-3/4"></div>
           <div className="skeleton h-4 w-full"></div>
