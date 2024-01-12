7e6134fff3cf96e365f17b037b4285dfef87d220
Aniva Bjorn <bearaknightly@gmail.com>
2024-01-12 13:24:19 -1000

    sixe fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <Navbar />
         <p className = "object-center font-mono">Explore the ruins of archiveterra while making your stamp on history</p>
          <span /> <span /> <span /> <span /> <span /> <span />
-         <div className="flex flex-col gap-4 w-auto git  object-center">
+         <div className="flex flex-col gap-4 w-4/5 git object-center">
           <div className="skeleton h-96 w-full"></div>
           <div className="skeleton h-4 w-4/5"></div>
           <div className="skeleton h-4 w-full"></div>
