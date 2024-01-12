5d175cf21d1530fae5939f56de72377156b8edba
7kkeen <2421200591@k12.hi.us>
2024-01-12 13:09:40 -1000

    change file upload color

--- src/pages/fileupload.tsx
+++ src/pages/fileupload.tsx
@@ -4,10 +4,10 @@ import Navbar from './navbar';
 export default function fileuploadpage() {
   return (
     <>
-      <main data-theme="retro" className='bg-primary'>
+      <main data-theme="retro" className='bg-neutral'>
         <Navbar />
         <h1 className = "font-mono text-justify-center">Upload thine files to the database if thou hast any</h1>
-        <input type="file" className="file-input file-justify-center file-input-bordered file-input-accent w-full max-w-xs" />
+        <input type="file" className="file-input file-justify-center bg-primary file-input-bordered file-input-accent w-full max-w-xs" />
         <div className="flex flex-col gap-4 w-52">
         <div className="skeleton h-32 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
