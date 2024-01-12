031f3ba238783b11b6706b02ad5a45c4e29a321f
Aniva Bjorn <bearaknightly@gmail.com>
2024-01-12 13:29:31 -1000

    size fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,13 +10,13 @@ export default function Home() {
     <>
        <main data-theme="retro" class-name = "bg-primary" >
         <Navbar />
-        <p className = "object-center font-mono">Explore the ruins of archiveterra while making your stamp on history</p>
+        <p className = "justify-self-center font-mono">Explore the ruins of archiveterra while making your stamp on history</p>
          <span /> <span /> <span /> <span /> <span /> <span />
-         <div className="flex flex-col gap-4 w-4/5 git object-center">
+         <div className="flex flex-col gap-4 w-4/5 git justify-self-center">
           <div className="skeleton h-96 w-full"></div>
+          <div className="skeleton h-4 w-3/4"></div>
+          <div className="skeleton h-4 w-full"></div>
           <div className="skeleton h-4 w-4/5"></div>
-          <div className="skeleton h-4 w-full"></div>
-          <div className="skeleton h-4 w-full"></div>
          </div>
        </main>
        
