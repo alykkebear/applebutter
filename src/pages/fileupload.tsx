b2406664d3ad0e7fa6126bc143319775718b1f3a
Aniva Bjorn <bearaknightly@gmail.com>
2024-01-12 13:27:04 -1000

    home button legends

--- src/pages/fileupload.tsx
+++ src/pages/fileupload.tsx
@@ -4,7 +4,7 @@ import Navbar from './navbar';
 export default function fileuploadpage() {
   return (
     <>
-      <main data-theme="retro" className='bg-accent'>
+      <main data-theme="retro" className='bg-base-200'>
         <Navbar />
         <h1 className = "font-mono text-justify-center">Upload thine files to the database if thou hast any</h1>
         <input type="file" className="file-input file-justify-center file-input-bordered file-input-accent w-full max-w-xs" />
