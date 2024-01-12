cb7c5e66aa5ea1fd17d887405417cec711e87198
7kkeen <2421200591@k12.hi.us>
2024-01-12 13:01:18 -1000

    change color

--- src/pages/fileupload.tsx
+++ src/pages/fileupload.tsx
@@ -4,7 +4,7 @@ import Navbar from './navbar';
 export default function fileuploadpage() {
   return (
     <>
-      <main data-theme="retro" className='bg-secondary'>
+      <main data-theme="retro" className='bg-primary'>
         <Navbar />
         <h1 className = "font-mono text-justify-center">Upload thine files to the database if thou hast any</h1>
         <input type="file" className="file-input file-justify-center file-input-bordered file-input-accent w-full max-w-xs" />
