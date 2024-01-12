b7d66b6d41ee5181de22f4e9c6d8d491371a5c0f
7kkeen <2421200591@k12.hi.us>
2024-01-12 13:06:31 -1000

    add skeleton

--- src/pages/fileupload.tsx
+++ src/pages/fileupload.tsx
@@ -8,6 +8,12 @@ export default function fileuploadpage() {
         <Navbar />
         <h1 className = "font-mono text-justify-center">Upload thine files to the database if thou hast any</h1>
         <input type="file" className="file-input file-justify-center file-input-bordered file-input-accent w-full max-w-xs" />
+        <div className="flex flex-col gap-4 w-52">
+        <div className="skeleton h-32 w-full"></div>
+        <div className="skeleton h-4 w-28"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-full"></div>
+        </div>
       </main>
     </>
      
