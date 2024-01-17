f6b1af4897c1e7ef5c44d50e6f60a3151570e519
7kkeen <2421200591@k12.hi.us>
2024-01-17 11:30:09 -1000

    change box

--- src/pages/fileupload.tsx
+++ src/pages/fileupload.tsx
@@ -8,11 +8,11 @@ export default function fileuploadpage() {
         <Navbar />
         <h1 className = "font-mono text-justify-center">Upload thine files to the database if thou hast any</h1>
         <input type="file" className="file-input file-justify-center file-input-bordered file-input-accent w-full max-w-xs" />
-        <div className="flex flex-col gap-4 w-52 ">
-        <div className="skeleton bg-primary h-32 w-full"></div>
-        <div className="skeleton bg-primary h-4 w-28"></div>
-        <div className="skeleton bg-primary h-4 w-full"></div>
-        <div className="skeleton bg-primary h-4 w-full"></div>
+        <div className="flex flex-col gap-4 w-4/5 justify-center items-center">
+          <div className="skeleton h-96 w-full"></div>
+          <div className="skeleton h-4 w-3/4"></div>
+          <div className="skeleton h-4 w-full"></div>
+          <div className="skeleton h-4 w-4/5"></div>
         </div>
       </main>
     </>
