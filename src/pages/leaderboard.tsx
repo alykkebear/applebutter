6178642d40e34fd7eaca597ca3919723d171c66a
7kkeen <2421200591@k12.hi.us>
2024-01-22 13:49:03 -1000

    spacing

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -28,17 +28,17 @@ export default function leaderboard() {
                     </tr>
                {/* row 2 */}
                     <tr>
-            <th>2</th>
-            <td>Hart Hagerty</td>
-            <td>Desktop Support Technician</td>
-            <td>Purple</td>
-        </tr>
-         {/* row 3 */}
-            <tr>
-            <th>3</th>
-            <td>Brice Swyre</td>
-            <td>Tax Accountant</td>
-            <td>Red</td>
+                    <th>2</th>
+                    <td>Hart Hagerty</td>
+                    <td>Desktop Support Technician</td>
+                    <td>Purple</td>
+                </tr>
+                {/* row 3 */}
+                <tr>
+                <th>3</th>
+                <td>Brice Swyre</td>
+                <td>Tax Accountant</td>
+                <td>Red</td>
         </tr>
     </tbody>
   </table>
