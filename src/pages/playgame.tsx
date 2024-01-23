ae2868fa212671762ba6dab15b5876088e2d3c85
Aniva Bjorn <bearaknightly@gmail.com>
2024-01-22 14:14:29 -1000

    rick

--- src/pages/playgame.tsx
+++ src/pages/playgame.tsx
@@ -4,9 +4,18 @@ import Navbar from "./navbar";
 export default function playgamepage() {
   return (
     <>
-      <main data-theme = "pastel">
+      <main data-theme = "retro">
         <Navbar />
-        <img src = '/rickyou.gif' className = 'h-screen w-screen flex items-center justify-center' />
+        <div className="hero font-mono min-h-screen" style={{backgroundImage: 'url(./rickyou.gif)'}}>
+          <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+              <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">Get Rickked</h1>
+              <p className="mb-5">jsut a placeholder for when we eventually have a super awesome epic game that you are going to enjoy so much it'll be epic and im running on my last brain cels hoping i ca finally get friggin tailwind to actually justify-center ಥ‿ಥ</p>
+              <button className="btn btn-primary">login (beta)</button>
+            </div>
+          </div>
+        </div>
      </main>
     </>
   );
