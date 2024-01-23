727748bcb1d988a061745e9740eac6a164c7f822
Aniva Bjorn <bearaknightly@gmail.com>
2024-01-22 14:29:07 -1000

    login page

---
+++ src/pages/loginbeta.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link';
+import React from 'react';
+import Navbar from "./navbar";
+export default function loginpage() {
+  return (
+    <>
+      <main data-theme = "retro">
+        <Navbar />
+        <div className="hero min-h-screen bg-base-200 font-mono " style={{backgroundImage: 'url(./cityslick.jpg)'}}>
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <p className="py-6">this will eventually work when we get a database...for now i guess we wait *sips tea* want some? its warm...</p>
+                </div>
+                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form className="card-body">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" placeholder="email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" placeholder="password" className="input input-bordered" required />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">i'm a doofus who cant remember a password</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Login</button>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div className="skeleton w-320 h-320"></div>
+     </main>
+    </>
+  );
+}
--- src/pages/playgame.tsx
+++ src/pages/playgame.tsx
@@ -12,7 +12,7 @@ export default function playgamepage() {
               <div className="max-w-md">
               <h1 className="mb-5 text-5xl font-bold">Get Rickked</h1>
               <p className="mb-5">jsut a placeholder for when we eventually have a super awesome epic game that you are going to enjoy so much it'll be epic and im running on my last brain cels hoping i ca finally get friggin tailwind to actually justify-center ಥ‿ಥ</p>
-              <button className="btn btn-primary">login (beta)</button>
+              <button className="btn btn-primary"><Link href = "./loginbeta" target = "_self">login (beta)</Link></button>
             </div>
           </div>
         </div>
