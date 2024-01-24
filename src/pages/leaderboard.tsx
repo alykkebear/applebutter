12e49dc3c7a59e0bc76fe0fbc50d95b7e5ee41b4
7kkeen <2421200591@k12.hi.us>
2024-01-24 11:37:48 -1000

    add scroll

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -98,132 +98,132 @@ export default function leaderboard() {
             <td>Accountant I</td> 
             <td>O'Hara, Welch and Keebler</td> 
             <td>Indonesia</td> 
-        <td>5/23/2020</td> 
-        <td>Crimson</td>
-        <th>8</th> 
+            <td>5/23/2020</td> 
+            <td>Crimson</td>
+            <th>8</th> 
       </tr>
-      <tr>
-        <th>9</th> 
-        <td>Lesya Tinham</td> 
-        <td>Safety Technician IV</td> 
-        <td>Turner-Kuhlman</td> 
-        <td>Philippines</td> 
-        <td>2/21/2021</td> 
-        <td>Maroon</td>
-        <th>9</th> 
+        <tr>
+            <th>9</th> 
+            <td>Lesya Tinham</td> 
+            <td>Safety Technician IV</td> 
+            <td>Turner-Kuhlman</td> 
+            <td>Philippines</td> 
+            <td>2/21/2021</td> 
+            <td>Maroon</td>
+            <th>9</th> 
       </tr>
-      <tr>
-        <th>10</th> 
-        <td>Zaneta Tewkesbury</td> 
-        <td>VP Marketing</td> 
-        <td>Sauer LLC</td> 
-        <td>Chad</td> 
-        <td>6/23/2020</td> 
-        <td>Green</td>
-        <th>10</th> 
+        <tr>
+            <th>10</th> 
+            <td>Zaneta Tewkesbury</td> 
+            <td>VP Marketing</td> 
+            <td>Sauer LLC</td> 
+            <td>Chad</td> 
+            <td>6/23/2020</td> 
+            <td>Green</td>
+            <th>10</th> 
       </tr>
-      <tr>
-        <th>11</th> 
-        <td>Andy Tipple</td> 
-        <td>Librarian</td> 
-        <td>Hilpert Group</td> 
-        <td>Poland</td> 
-        <td>7/9/2020</td> 
-        <td>Indigo</td>
-        <th>11</th> 
+        <tr>
+            <th>11</th> 
+            <td>Andy Tipple</td> 
+            <td>Librarian</td> 
+            <td>Hilpert Group</td> 
+            <td>Poland</td> 
+            <td>7/9/2020</td> 
+            <td>Indigo</td>
+            <th>11</th> 
       </tr>
-      <tr>
-        <th>12</th> 
-        <td>Sophi Biles</td> 
-        <td>Recruiting Manager</td> 
-        <td>Gutmann Inc</td> 
-        <td>Indonesia</td> 
-        <td>2/12/2021</td> 
-        <td>Maroon</td>
-        <th>12</th> 
+        <tr>
+            <th>12</th> 
+            <td>Sophi Biles</td> 
+            <td>Recruiting Manager</td> 
+            <td>Gutmann Inc</td> 
+            <td>Indonesia</td> 
+            <td>2/12/2021</td> 
+            <td>Maroon</td>
+            <th>12</th> 
       </tr>
-      <tr>
-        <th>13</th> 
-        <td>Florida Garces</td> 
-        <td>Web Developer IV</td> 
-        <td>Gaylord, Pacocha and Baumbach</td> 
-        <td>Poland</td> 
-        <td>5/31/2020</td> 
-        <td>Purple</td>
-        <th>13</th> 
+        <tr>
+            <th>13</th> 
+            <td>Florida Garces</td> 
+            <td>Web Developer IV</td> 
+            <td>Gaylord, Pacocha and Baumbach</td> 
+            <td>Poland</td> 
+            <td>5/31/2020</td> 
+            <td>Purple</td>
+            <th>13</th> 
       </tr>
-      <tr>
-        <th>14</th> 
-        <td>Maribeth Popping</td> 
-        <td>Analyst Programmer</td> 
-        <td>Deckow-Pouros</td> 
-        <td>Portugal</td> 
-        <td>4/27/2021</td> 
-        <td>Aquamarine</td>
-        <th>14</th> 
+        <tr>
+            <th>14</th> 
+            <td>Maribeth Popping</td> 
+            <td>Analyst Programmer</td> 
+            <td>Deckow-Pouros</td> 
+            <td>Portugal</td> 
+            <td>4/27/2021</td> 
+            <td>Aquamarine</td>
+            <th>14</th> 
       </tr>
-      <tr>
-        <th>15</th> 
-        <td>Moritz Dryburgh</td> 
-        <td>Dental Hygienist</td> 
-        <td>Schiller, Cole and Hackett</td> 
-        <td>Sri Lanka</td> 
-        <td>8/8/2020</td> 
-        <td>Crimson</td>
-        <th>15</th> 
+        <tr>
+            <th>15</th> 
+            <td>Moritz Dryburgh</td> 
+            <td>Dental Hygienist</td> 
+            <td>Schiller, Cole and Hackett</td> 
+            <td>Sri Lanka</td> 
+            <td>8/8/2020</td> 
+            <td>Crimson</td>
+            <th>15</th> 
       </tr>
-      <tr>
-        <th>16</th> 
-        <td>Reid Semiras</td> 
-        <td>Teacher</td> 
-        <td>Sporer, Sipes and Rogahn</td> 
-        <td>Poland</td> 
-        <td>7/30/2020</td> 
-        <td>Green</td>
-        <th>16</th> 
+        <tr>
+            <th>16</th> 
+            <td>Reid Semiras</td> 
+            <td>Teacher</td> 
+            <td>Sporer, Sipes and Rogahn</td> 
+            <td>Poland</td> 
+            <td>7/30/2020</td> 
+            <td>Green</td>
+            <th>16</th> 
       </tr>
-      <tr>
-        <th>17</th> 
-        <td>Alec Lethby</td> 
-        <td>Teacher</td> 
-        <td>Reichel, Glover and Hamill</td> 
-        <td>China</td> 
-        <td>2/28/2021</td> 
-        <td>Khaki</td>
-        <th>17</th> 
+        <tr>
+            <th>17</th> 
+            <td>Alec Lethby</td> 
+            <td>Teacher</td> 
+            <td>Reichel, Glover and Hamill</td> 
+            <td>China</td> 
+            <td>2/28/2021</td> 
+            <td>Khaki</td>
+            <th>17</th> 
       </tr>
-      <tr>
-        <th>18</th> 
-        <td>Aland Wilber</td> 
-        <td>Quality Control Specialist</td> 
-        <td>Kshlerin, Rogahn and Swaniawski</td> 
-        <td>Czech Republic</td> 
-        <td>9/29/2020</td> 
-        <td>Purple</td>
-        <th>18</th> 
+        <tr>
+            <th>18</th> 
+            <td>Aland Wilber</td> 
+            <td>Quality Control Specialist</td> 
+            <td>Kshlerin, Rogahn and Swaniawski</td> 
+            <td>Czech Republic</td> 
+            <td>9/29/2020</td> 
+            <td>Purple</td>
+            <th>18</th> 
       </tr>
-      <tr>
-        <th>19</th> 
-        <td>Teddie Duerden</td> 
-        <td>Staff Accountant III</td> 
-        <td>Pouros, Ullrich and Windler</td> 
-        <td>France</td> 
-        <td>10/27/2020</td> 
-        <td>Aquamarine</td>
-        <th>19</th> 
+        <tr>
+            <th>19</th> 
+            <td>Teddie Duerden</td> 
+            <td>Staff Accountant III</td> 
+            <td>Pouros, Ullrich and Windler</td> 
+            <td>France</td> 
+            <td>10/27/2020</td> 
+            <td>Aquamarine</td>
+            <th>19</th> 
       </tr>
-      <tr>
-        <th>20</th> 
-        <td>Lorelei Blackstone</td> 
-        <td>Data Coordinator</td> 
-        <td>Witting, Kutch and Greenfelder</td> 
-        <td>Kazakhstan</td> 
-        <td>6/3/2020</td> 
-        <td>Red</td>
-        <th>20</th> 
+        <tr>
+            <th>20</th> 
+            <td>Lorelei Blackstone</td> 
+            <td>Data Coordinator</td> 
+            <td>Witting, Kutch and Greenfelder</td> 
+            <td>Kazakhstan</td> 
+            <td>6/3/2020</td> 
+            <td>Red</td>
+            <th>20</th> 
       </tr>
     </tbody> 
-    <tfoot>
+   <tfoot>
       <tr>
         <th></th> 
         <td>Name</td> 
