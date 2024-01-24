b414293227fe00ada5aabbc33a54eeabda07f6ed
7kkeen <2421200591@k12.hi.us>
2024-01-24 11:21:04 -1000

    pls fix format

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -18,91 +18,91 @@ export default function leaderboard() {
                         <th></th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr>
-                    <th>1</th>
-                    <td>Cy Ganderton</td>
-                    <td>120</td>
-                    <td>2000</td>
+                    <tbody>
+                        <tr>
+                        <th>1</th>
+                        <td>Cy Ganderton</td>
+                        <td>120</td>
+                        <td>2000</td>
                   </tr>
-                <tr>
-                    <th>2</th>
-                    <td>Hart Hagerty</td>
-                    <td>100</td>
-                    <td>512</td>
-                  </tr>
-                <tr>
-                     <th>3</th>
-                     <td>Brice Swyre</td>
-                     <td>1000</td>
-                     <td></td>
-                  </tr>
-                <tr>
-                    <th>4</th>
-                    <td>Brice Swyre</td>
-                    <td>Tax Accountant</td>
-                    <td>Red</td>
+                        <tr>
+                        <th>2</th>
+                        <td>Hart Hagerty</td>
+                        <td>100</td>
+                        <td>512</td>
                  </tr>
-                <tr>
-                    <th>5</th>
-                    <td>Brice Swyre</td>
-                    <td>Tax Accountant</td>
-                    <td>Red</td>
+                        <tr>
+                        <th>3</th>
+                        <td>Brice Swyre</td>
+                        <td>1000</td>
+                        <td></td>
+                 </tr>
+                        <tr>
+                        <th>4</th>
+                        <td>Brice Swyre</td>
+                        <td>Tax Accountant</td>
+                        <td>Red</td>
+                 </tr>
+                        <tr>
+                        <th>5</th>
+                        <td>Brice Swyre</td>
+                        <td>Tax Accountant</td>
+                        <td>Red</td>
                   </tr>
-                <tr>
-                    <th>6</th>
-                    <td>Brice Swyre</td>
-                    <td>Tax Accountant</td>
-                    <td>Red</td>
+                        <tr>
+                        <th>6</th>
+                        <td>Brice Swyre</td>
+                        <td>Tax Accountant</td>
+                        <td>Red</td>
                   </tr>
-                <tr>
-                    <th>7</th>
-                    <td>Brice Swyre</td>
-                    <td>Tax Accountant</td>
-                    <td>Red</td>
+                        <tr>
+                        <th>7</th>
+                        <td>Brice Swyre</td>
+                        <td>Tax Accountant</td>
+                        <td>Red</td>
                   </tr>
-                <tr>
-                    <th>8</th>
-                    <td>Brice Swyre</td>
-                    <td>Tax Accountant</td>
-                    <td>Red</td>
+                        <tr>
+                        <th>8</th>
+                        <td>Brice Swyre</td>
+                        <td>Tax Accountant</td>
+                        <td>Red</td>
                   </tr>
-                <tr>
-                    <th>9</th>
-                    <td>Brice Swyre</td>
-                    <td>Tax Accountant</td>
-                    <td>Red</td>
+                        <tr>
+                        <th>9</th>
+                        <td>Brice Swyre</td>
+                        <td>Tax Accountant</td>
+                        <td>Red</td>
                   </tr>
-                <tr>
-                    <th>10</th>
-                    <td>Brice Swyre</td>
-                    <td>Tax Accountant</td>
-                    <td>Red</td>
+                        <tr>
+                        <th>10</th>
+                        <td>Brice Swyre</td>
+                        <td>Tax Accountant</td>
+                        <td>Red</td>
                   </tr>
-                <tr>
-                    <th>11</th>
-                    <td>Brice Swyre</td>
-                    <td>Tax Accountant</td>
-                    <td>Red</td>
+                        <tr>
+                        <th>11</th>
+                        <td>Brice Swyre</td>
+                        <td>Tax Accountant</td>
+                        <td>Red</td>
                   </tr>
-                <tr>
-                    <th>12</th>
-                    <td>Brice Swyre</td>
-                    <td>Tax Accountant</td>
-                    <td>Red</td>
+                        <tr>
+                        <th>12</th>
+                        <td>Brice Swyre</td>
+                        <td>Tax Accountant</td>
+                        <td>Red</td>
                   </tr>
                 </tbody>
                 <tfoot>
-                <tr>
-                    <th></th> 
-                    <td>Name</td> 
-                    <td>Level</td> 
-                   <td>Score</td> 
-                   <th></th>
-    </tr>
-  </tfoot>
-</table>
-</div>
+                        <tr>
+                        <th></th> 
+                        <td>Name</td> 
+                        <td>Level</td> 
+                        <td>Score</td> 
+                        <th></th>
+                  </tr>
+                </tfoot>
+            </table>
+        </div>
 </main>
 </>   
     );
