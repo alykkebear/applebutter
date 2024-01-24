77e80291df7c3286a610014484a22a80968a8cef
7kkeen <2421200591@k12.hi.us>
2024-01-24 11:46:36 -1000

    plsss work

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -22,52 +22,52 @@ export default function leaderboard() {
             </tr>
         </thead> 
       <tbody>
-        <tr>
-            <th>1</th> 
-            <td>Cy Ganderton</td> 
-            <td>Quality Control Specialist</td> 
-            <td>Littel, Schaden and Vandervort</td> 
-            <td>Canada</td> 
-            <td>12/16/2020</td> 
-            <td>Blue</td>
-            <th>1</th> 
-      </tr>
-        <tr>
-            <th>2</th> 
-            <td>Hart Hagerty</td> 
-            <td>Desktop Support Technician</td> 
-            <td>Zemlak, Daniel and Leannon</td> 
-            <td>United States</td> 
-            <td>12/5/2020</td> 
-            <td>Purple</td>
-            <th>2</th> 
-     </tr>
-        <tr>
-            <th>3</th> 
-            <td>Brice Swyre</td> 
-            <td>Tax Accountant</td> 
-            <td>Carroll Group</td> 
-            <td>China</td> 
-            <td>8/15/2020</td> 
-            <td>Red</td>
-            <th>3</th> 
-      </tr>
-        <tr>
-            <th>4</th> 
-            <td>Marjy Ferencz</td> 
-            <td>Office Assistant I</td> 
-            <td>Rowe-Schoen</td> 
-            <td>Russia</td> 
-            <td>3/25/2021</td> 
-            <td>Crimson</td>
-            <th>4</th> 
-      </tr>
-        <tr>
-            <th>5</th> 
-            <td>Yancy Tear</td> 
-            <td>Community Outreach Specialist</td> 
-            <td>Wyman-Ledner</td> 
-            <td>Brazil</td> 
+             <tr>
+                <th>1</th> 
+                <td>Cy Ganderton</td> 
+                <td>Quality Control Specialist</td> 
+                <td>Littel, Schaden and Vandervort</td> 
+                <td>Canada</td> 
+                <td>12/16/2020</td> 
+                <td>Blue</td>
+                <th>1</th> 
+            </tr>
+             <tr>
+                <th>2</th> 
+                <td>Hart Hagerty</td> 
+                <td>Desktop Support Technician</td> 
+                <td>Zemlak, Daniel and Leannon</td> 
+                <td>United States</td> 
+                <td>12/5/2020</td> 
+                <td>Purple</td>
+                <th>2</th> 
+           </tr>
+            <tr>
+                <th>3</th> 
+                <td>Brice Swyre</td> 
+                <td>Tax Accountant</td> 
+                <td>Carroll Group</td> 
+                <td>China</td> 
+                <td>8/15/2020</td> 
+                <td>Red</td>
+                <th>3</th> 
+           </tr>
+            <tr>
+                <th>4</th> 
+                <td>Marjy Ferencz</td> 
+                <td>Office Assistant I</td> 
+                <td>Rowe-Schoen</td> 
+                <td>Russia</td> 
+                <td>3/25/2021</td> 
+                <td>Crimson</td>
+                <th>4</th> 
+           </tr>
+            <tr>
+                <th>5</th> 
+                <td>Yancy Tear</td> 
+                <td>Community Outreach Specialist</td> 
+                <td>Wyman-Ledner</td> 
+                <td>Brazil</td> 
             <td>5/22/2020</td> 
             <td>Indigo</td>
             <th>5</th> 
