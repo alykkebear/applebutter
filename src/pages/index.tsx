d2b9778857e1b19c299c3c20664e0a1179ebe545
Aniva Bjorn <bearaknightly@gmail.com>
2024-04-05 13:30:22 -1000

    update database

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
                 <h1 className="mb-5 text-5xl font-bold">Welcome to ArchiveTerra</h1>
                 <p className="mb-5">Archiveterra is a state-of-the art system meant to make archiving fun!!! IK it lookes like crap now but come back in a few months and it'll probably work. Believe in teh belief that it can worrrrkkkkkk idk this is just filler text tbh. i really wanna buy a resin printer ~</p>
                 <button className="btn btn-primary"><Link href = "./playgame" target = "_self">Play</Link></button>
-                <p>hello</p>
+
                
               </div>
             </div>
