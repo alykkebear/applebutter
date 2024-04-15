aaa8e9fe27b1cf3c86a4faf18de3e31180a0140f
Aniva Bjorn <bearaknightly@gmail.com>
2024-04-15 13:02:45 -1000

    the magic code saves alll

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,14 +20,17 @@ export default function Home() {
               <div className="max-w-md">
                 <h1 className="mb-5 text-5xl font-bold">Welcome to ArchiveTerra</h1>
                 <p className="mb-5">Archiveterra is a state-of-the art system meant to make archiving fun!!! IK it lookes like crap now but come back in a few months and it'll probably work. Believe in teh belief that it can worrrrkkkkkk idk this is just filler text tbh. i really wanna buy a resin printer ~</p>
-                <button className="btn btn-primary"><Link href = "./playgame" target = "_self">Play</Link></button>
-
-               
+                  <div>
+                    <div className="chat chat-start">
+                     <div className="chat-bubble bg-secondary">{data?.map(({id, content}) => (<div key = {id}>{content}</div>))}</div>
+                      </div>
+                   <div className="chat chat-end">
+                    <div className="chat-bubble bg-primary">You underestimate my power!</div>
+                  </div>
+                  </div>
+                <button className="btn btn-primary"><Link href = "./playgame" target = "_self">Play</Link></button>    
               </div>
             </div>
-          </div>
-          <div>
-            {data?.map(({id, content}) => (<div key = {id}>{content}</div>))}
           </div>
           <Footer />
        </main>
@@ -35,3 +38,4 @@ export default function Home() {
     </>
   );
 }
+//the magic codeeee {data?.map(({id, content}) => (<div key = {id}>{content}</div>))} ⊹⋛⋋( ՞ਊ ՞)⋌⋚⊹
