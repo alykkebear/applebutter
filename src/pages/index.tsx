6e68fde374879df53797dcedca46652d9b8b1722
Aniva Bjorn <bearaknightly@gmail.com>
2024-04-15 14:03:10 -1000

    two column

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,16 +19,16 @@ export default function Home() {
             <div className="hero-content text-center text-neutral-content">
               <div className="max-w-md">
                 <h1 className="mb-5 text-5xl font-bold">Welcome to ArchiveTerra</h1>
-                <p className="mb-5">Archiveterra is a state-of-the art system meant to make archiving fun!!! IK it lookes like crap now but come back in a few months and it'll probably work. Believe in teh belief that it can worrrrkkkkkk idk this is just filler text tbh. i really wanna buy a resin printer ~</p>
+                <p className="mb-5">Archiveterra is a state-of-the art system meant to make archiving fun!!! IK it lookes like crap now but come back in a few months and it'll probably work. Believe in teh belief that it can worrrrkkkkkk idk this is just filler text tbh. i really wanna buy a resin printer ~/br</p>
                   <div>
+                    <div> </div>
                     <div className="chat chat-start">
-                     <div className="chat-bubble bg-secondary">{data?.map(({id, content}) => (<div key = {id}>{content}</div>))}</div>
+                     <div className="chat-bubble bg-secondary">{data?.map(({id, authorId, content}) => (<div key = {id}>{authorId}:{content}</div>))}</div>
                       </div>
                    <div className="chat chat-end">
                     <div className="chat-bubble bg-primary">You underestimate my power!</div>
                   </div>
-                  </div>
-                <button className="btn btn-primary"><Link href = "./playgame" target = "_self">Play</Link></button>    
+                  </div>   
               </div>
             </div>
           </div>
@@ -39,3 +39,4 @@ export default function Home() {
   );
 }
 //the magic codeeee {data?.map(({id, content}) => (<div key = {id}>{content}</div>))} ⊹⋛⋋( ՞ਊ ՞)⋌⋚⊹
+//the magic command npx prisma studio you're welcome
