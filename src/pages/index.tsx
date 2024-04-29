e26cf419a33efe6b02381840f3e3da3b2f10861f
Aniva Bjorn <bearaknightly@gmail.com>
2024-04-29 13:56:28 -1000

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -66,21 +66,7 @@ export default function Home() {
                       </div>
 
                       <div>
-                      {data?.map(({id, authorId, content}) =>
-                        (
-                          <div 
-                            key = {id}>
-                            
-                          
-                        
-                          <div className="chat chat-start">
-                               <div className="chat-bubble">It's over Anakin, <br/>I have the high ground.</div>
-                          </div>
-                          <div className="chat chat-end">
-                               <div className="chat-bubble">You underestimate my power!</div>
-                          </div>
-
-                          ))}
+                      
 
                      </div>
 
